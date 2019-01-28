a1d7b10062bdcebd5fad343f00740885033f9956
unknown <38929577+meginks@users.noreply.github.com>
2019-01-28 14:41:53 -0600

    added header and footer text

--- js/index.js
+++ js/index.js
@@ -40,3 +40,44 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav 
+
+let nav1 = document.getElementsByTagName("a")[0].setAttribute("id", "nav-item-1"); 
+nav1 = document.getElementById("nav-item-1"); 
+nav1.textContent = siteContent.nav["nav-item-1"];
+
+let nav2 = document.getElementsByTagName("a")[1].setAttribute("id", "nav-item-2"); 
+nav2 = document.getElementById("nav-item-2");
+nav2.textContent = siteContent.nav["nav-item-2"]; 
+
+let nav3 = document.getElementsByTagName("a")[2].setAttribute("id", "nav-item-3"); 
+nav3 = document.getElementById("nav-item-3"); 
+nav3.textContent = siteContent.nav["nav-item-3"]; 
+
+let nav4 = document.getElementsByTagName("a")[3].setAttribute("id", "nav-item-4");
+nav4 = document.getElementById("nav-item-4");
+nav4.textContent = siteContent.nav["nav-item-4"]; 
+
+let nav5 = document.getElementsByTagName("a")[4].setAttribute("id", "nav-item-5");
+nav5 = document.getElementById("nav-item-5");
+nav5.textContent = siteContent.nav["nav-item-5"];
+
+let nav6 = document.getElementsByTagName("a")[5].setAttribute("id", "nav-item-6");
+nav6 = document.getElementById("nav-item-6");
+nav6.textContent = siteContent.nav["nav-item-6"];
+
+
+// main content 
+let contactHeader = document.querySelector(".contact h4");
+
+contactHeader.textContent = siteContent.contact["contact-h4"]; 
+
+
+
+
+
+//footer 
+let footer = document.querySelector("footer p");
+
+footer.textContent = siteContent.footer["copyright"];
