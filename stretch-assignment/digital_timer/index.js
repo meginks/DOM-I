8dee338a5ec1c030c0f5e7f14d946379454190d4
unknown <38929577+meginks@users.noreply.github.com>
2019-01-28 18:27:56 -0600

    stretch attempt

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,7 @@
+let msTens = document.getElementById("msTens");
+let msHundreds = document.getElementById("msHundreds");
+let colon = document.getElementById("colon");
+let secondOnes = document.getElementById("secondOnes");
+let secondTens = document.getElementById("secondTens");
+
+
